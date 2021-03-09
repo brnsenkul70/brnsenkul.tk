c6ec3ea9a760e23f521b5c29b3e667b80446621e
Baran Toprak Şenkul <brnsenkul70@gmail.com>
2021-03-09 18:49:26 +0300

    brnsenkul.tk V1.01
    Finally completed...

--- js.js
+++ js.js
@@ -1,14 +1,8 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
+      x.className = "topnav";
+    }
+  }
